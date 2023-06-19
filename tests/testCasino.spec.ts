abb65e39440ce542a99a2af239b6a7920589949a
María Emilia Landa <emilia.landa@gmail.com>
2023-06-19 17:32:51 -0300

    cambios en tests

--- tests/testCasino.spec.ts
+++ tests/testCasino.spec.ts
@@ -5,7 +5,7 @@ const app = require('../src/app.ts')
 
 describe('Casino API', () => {
 
-  //------------------test ruta get-----------------// 
+  //------------------test ruta get / -----------------// 
   test('Debería obtener todos los casinos', async () => {
     // Realiza la solicitud GET utilizando Supertest y el enrutador de casino
     const response = await request(casinoRouter()).get('/');
@@ -13,10 +13,33 @@ describe('Casino API', () => {
     // Verifica que la respuesta tenga el código de estado correcto
     expect(response.statusCode).toBe(200);
 
-    // Verifica el contenido de la respuesta (opcional)
+    // Verifica el contenido de la respuesta 
     expect(response.body).toBeDefined();
     expect(Array.isArray(response.body)).toBe(true);
   });
+
+    //------------------test ruta get /:id -----------------// 
+  test('Debería obtener un casino por su ID', async () => {
+      const casinoId = '1'; 
+      const response = await request(app).get(`/casino/${casinoId}`);
+      expect(response.status).toBe(200);
+  
+      expect(response.body).toBeDefined();
+      expect(response.body.id).toBe(casinoId);
+    });
+  
+   test('Debería retornar un error si el ID del casino no existe', async () => {
+      const casinoId = '999'; // ID del casino inexistente
+      const response = await request(app).get(`/casino/${casinoId}`);
+
+      expect(response.status).toBe(404);
+  
+      // Verifica el contenido de la respuesta (opcional)
+      expect(response.body).toBeDefined();
+      expect(response.body.error).toBe(`El casino con ID ${casinoId} no existe.`);
+    });
+  });  
+
 
   //----------------test ruta post--------------------//
 
