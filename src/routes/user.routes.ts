38bc1b5c040a2ac99da2df453e266bc4f63722ae
María Emilia Landa <emilia.landa@gmail.com>
2023-06-19 20:06:49 -0300

    cambios sin middleware

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -9,7 +9,7 @@ export default function usersRouter(): Router {
   return Router()
 
     .get('/:id', handler.findOneById)
-    .get("/",authMiddleware.authorize, handler.getAll)
+    .get("/", handler.getAll)
     .post("/", handler.create)
     .post("/login",handler.logIn)
     .delete("/:id", handler.delete)
