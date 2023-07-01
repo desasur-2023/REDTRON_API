c3bf786fa75c1879dc100f07959baf6da50136a8
Federico Interlandi Zoireff <ing.finterlandi@gmail.com>
2023-07-01 11:05:42 -0300

    Cambios necesarios para actualizar casino domain con relaciones db

--- src/models/casino.model.ts
+++ src/models/casino.model.ts
@@ -3,17 +3,15 @@ import { Casino } from "../domain/casino";
 
 @Entity('casino')
 export class CasinoEntity implements Casino{
+    
     @PrimaryGeneratedColumn('uuid')
     id: string;
     
     @Column({name: "name",type: "varchar", length: 32, nullable: false})
     name: string;
-    
-    @Column({name: "profits",type: "decimal", precision: 10, scale: 2,nullable: false})
-    profits: number;
 
-    @Column({name: "losses",type: "decimal", precision: 10, scale: 2, nullable: false})
-    losses: number;
+    @Column({name: "image_url",type: "varchar", length: 255, nullable: true})
+    imageUrl: string;
     
     @Column({name: "created_at",type: "timestamp",nullable: false,default: () => "now()",})
     createdAt: Date;
