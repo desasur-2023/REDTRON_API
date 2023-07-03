f9718523cf2c0da76092ca37dbe0fcd51e794514
HugoSchierano <hugoschierano@gmail.com>
2023-07-03 15:56:10 -0300

    Comprobacion de que un usuario y un casino solo pueden estar asociados una vez

--- src/controllers/userCasino.controller.ts
+++ src/controllers/userCasino.controller.ts
@@ -10,47 +10,51 @@ import { Casino } from "../domain/casino";
 
 
 
-const create  = async (userId: string, casinoId: string) => {
-    const userCasinoDAO = await new UserCasinoDAO()
+const create = async (userId: string, casinoId: string) => {
+  const userCasinoDAO = await new UserCasinoDAO()
 
-    if (!isValidUUID(casinoId)) {
-        throw new BaseError('Invalid casinoId format', StatusCodes.BAD_REQUEST);
-      }
+  if (!isValidUUID(casinoId)) {
+    throw new BaseError('Invalid casinoId format', StatusCodes.BAD_REQUEST);
+  }
 
-    if (!isValidUUID(userId)) {
-        throw new BaseError('Invalid userid format', StatusCodes.BAD_REQUEST);
-      }
-    
-    const userDAO = await new UserDAO();
-    const user =  await userDAO.read(userId).catch(error => new BaseError("Usuario inexistente", StatusCodes.CONFLICT, error.message));
-    
-    if(Object.keys(user).length === 0){
-        throw new BaseError(`The id: '${userId}' does not belong to an existing casino.`, StatusCodes.CONFLICT);
-    }
+  if (!isValidUUID(userId)) {
+    throw new BaseError('Invalid userid format', StatusCodes.BAD_REQUEST);
+  }
 
-    const casinoDAO = await new CasinoDAO();
-    const casino = await casinoDAO.read(casinoId).catch(error => new BaseError(`The id: '${casinoId}' does not belong to an existing casino.`, StatusCodes.CONFLICT, error.message));
-   
-    
-    if(Object.keys(casino).length === 0){
-        throw new BaseError(`The id: '${casino}' does not belong to an existing casino.`, StatusCodes.CONFLICT);
-    }
+  const checkUserCasino = await userCasinoDAO.search(userId, casinoId).catch(error => new BaseError('The ids does not belong to an existing user or casino.', StatusCodes.CONFLICT, error.message));
+  
+  if(Object.keys(checkUserCasino).length !== 0) throw new BaseError('The user has already assigned to this casino', StatusCodes.CONFLICT);
 
-    const userCasino = await new User_Casino_Entity();
-    userCasino.user = userId as unknown as User
-    userCasino.casino = casinoId as unknown as Casino
-    userCasino.credits = 0
-    userCasino.debits = 0
+  const userDAO = await new UserDAO();
+  const user = await userDAO.read(userId).catch(error => new BaseError(`The id: '${userId}' does not belong to an existing user.`, StatusCodes.CONFLICT, error.message));
 
-    const result =  await userCasinoDAO.create(userCasino).catch(error => new BaseError("Failed to create the user_casino.", StatusCodes.CONFLICT));
+  if (Object.keys(user).length === 0) {
+    throw new BaseError(`The id: '${userId}' does not belong to an existing user.`, StatusCodes.CONFLICT);
+  }
 
-   return result
+  const casinoDAO = await new CasinoDAO();
+  const casino = await casinoDAO.read(casinoId).catch(error => new BaseError(`The id: '${casinoId}' does not belong to an existing casino.`, StatusCodes.CONFLICT, error.message));
+  const casinoName = casino?.name;
+
+  if (Object.keys(casino).length === 0) {
+    throw new BaseError(`The id: '${casino}' does not belong to an existing casino.`, StatusCodes.CONFLICT);
+  }
+
+  const userCasino = await new User_Casino_Entity();
+  userCasino.user = userId as unknown as User
+  userCasino.casino = casinoId as unknown as Casino
+  userCasino.credits = 0
+  userCasino.debits = 0
+
+  const result = await userCasinoDAO.create(userCasino).catch(error => new BaseError("Failed to create the user_casino.", StatusCodes.CONFLICT));
+
+  return result;
 }
 
-const getAll = async (user?:string, casino?:string) => {
+const getAll = async (user?: string, casino?: string) => {
   const userDAO = await new UserCasinoDAO();
-  const result =  await userDAO.search(user, casino).catch(error => new BaseError("No User_Casino found with those parameters", StatusCodes.CONFLICT, error.message));
-  if(result instanceof BaseError) throw result;
+  const result = await userDAO.search(user, casino).catch(error => new BaseError("No User_Casino found with those parameters", StatusCodes.CONFLICT, error.message));
+  if (result instanceof BaseError) throw result;
 
   return result;
 };
